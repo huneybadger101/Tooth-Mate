d7dd657074569e21e7c5b9409fd089feae40d8c5
MatthewPiersonUni <83790784+MatthewPiersonUni@users.noreply.github.com>
2022-04-30 15:59:50 +1200

    Create initial react-native application

---
+++ dentalsoftware/windows/dentalsoftware/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::dentalsoftware::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::dentalsoftware::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
