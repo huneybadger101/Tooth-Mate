6d8addafbb8a1933abea2adada70b2fdd606ad4a
MatthewPiersonUni <83790784+MatthewPiersonUni@users.noreply.github.com>
2022-05-10 18:34:47 +1200

    Formatting

--- dentalsoftware_frontend/src/components/tabContainer.tsx
+++ dentalsoftware_frontend/src/components/tabContainer.tsx
@@ -5,10 +5,12 @@ import React, { useState } from "react";
 function TabContainer(props: any) {
 
     const buttonHandler = (newView: any) => {
-        setValues({
-            children: state.children,
-            view: newView,
-        })
+        setValues(
+            {
+                children: state.children,
+                view: newView,
+            }
+        )
     }
     var childrenArray = [];
     if (Object.prototype.toString.call(props.children) != '[object Array]') {
@@ -19,26 +21,39 @@ function TabContainer(props: any) {
         childrenArray = props.children;
     }
     // Set state variables
-    const [state, setValues] = useState({
-        children: childrenArray,
-        view: childrenArray[0],
-    });
+    const [state, setValues] = useState(
+        {
+            children: childrenArray,
+            view: childrenArray[0],
+        }
+    );
 
     var tabs: { tabName: string; view: any; }[] = [];
     var tabIndex = 0;
     // Go from state array to local array to access .map
     state.children.forEach(function (value: any) {
-        tabs.push({
-            tabName: "TAB" + tabIndex++,
-            view: value,
-        })
+        tabs.push(
+            {
+                tabName: "TAB" + tabIndex++,
+                view: value,
+            }
+        )
     });
     // Create header with provided tabs
     var header = <View id="header" style="flex: 1; flex-direction: 'row'; justify-content: 'center'; align-items: 'center'; background-color: 'pink'; position: 'absolute'; top: 0px; height: 60px; right: 0px; left: 0px;">
-                    {tabs.map((value: any) => {
-                        // Eventually style these to be proper tabs, buttons are fine for now
-                        return <Button text={value.tabName} on={{[WidgetEventTypes.MouseButtonRelease]: /*Only trigger when left click is released*/ () => buttonHandler(value.view), [WidgetEventTypes.MouseButtonPress]: /*Only trigger when left click is pressed*/ () => console.log("Pressed " + value.tabName)}}/>
-                    })}
+                    {
+                        tabs.map(
+                            (value: any) => {
+                                // Eventually style these to be proper tabs, buttons are fine for now
+                                return <Button text={value.tabName} on={
+                                    {
+                                        [WidgetEventTypes.MouseButtonRelease]: /*Only trigger when left click is released*/ () => buttonHandler(value.view), 
+                                        [WidgetEventTypes.MouseButtonPress]: /*Only trigger when left click is pressed*/ () => console.log("Pressed " + value.tabName)
+                                    }
+                                }/>
+                            }
+                        )
+                    }
                 </View>;
     // Display the view
     return (
