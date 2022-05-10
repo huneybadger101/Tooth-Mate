7355cbaa299a5f1addfc27decce06458d50d8835
MatthewPiersonUni <83790784+MatthewPiersonUni@users.noreply.github.com>
2022-05-10 15:17:19 +1200

    Remove React Native and swap to React NodeGUI

---
+++ dentalsoftware_frontend/src/app.tsx
@@ -0,0 +1,52 @@
+import { Text, Window, hot, View } from "@nodegui/react-nodegui";
+import React from "react";
+import { QIcon } from "@nodegui/nodegui";
+import path from "path";
+import Homepage from "./components/homepage";
+import nodeguiIcon from "../assets/nodegui.jpg";
+
+const minSize = { width: 1000, height: 520 };
+const winIcon = new QIcon(path.resolve(__dirname, nodeguiIcon));
+class App extends React.Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      currentScreen: <Homepage/>,
+    }
+  }
+  // Reference to this function needs to be passed to each component in order for
+  // this App component to display sub-components
+  updateCurrentScreenComponent(newView: React.Component) {
+    this.setState({
+      currentScreen: newView,
+    })
+  }
+
+  // Function that returns a component to be drawn, can have children components if the parent component supports it
+  render() {
+
+    // Must wrap main App component in a React.Fragment component
+    // in order to allow for sub-windows to be created later on
+    return (
+      <React.Fragment> 
+        <Window
+          windowIcon={winIcon}
+          windowTitle="ToothMate Dental Software"
+          minSize={minSize}
+        >
+          <View style={containerStyle}>
+            {this.state.currentScreen}
+          </View>
+          
+        </Window>
+      </React.Fragment>
+    );
+  }
+}
+
+const containerStyle = `
+  flex: 1; 
+`;
+
+export default hot(App);
---
+++ dentalsoftware_frontend/src/components/homepage.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "@nodegui/react-nodegui";
+import React from "react";
+
+export class Homepage extends React.Component {
+
+    // Function that returns a component to be drawn, can have children components if the parent component supports it
+    render() {
+        
+        return (
+            <View style="flex: 1; justify-content: 'center'; align-items: 'center'; background-color: 'grey';">
+                <Text wordWrap={true} style="color: 'black'; font-size: 35px;">
+                    Homepage Component
+                </Text>
+            </View>
+        );
+    }
+} 
+
+export default Homepage;
