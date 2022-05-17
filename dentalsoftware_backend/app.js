ff1c1707e96e3cb022a86b5ad59402e779c72317
MatthewPiersonUni <83790784+MatthewPiersonUni@users.noreply.github.com>
2022-05-17 15:00:57 +1200

    Add in ability to create tables

--- dentalsoftware_backend/app.js
+++ dentalsoftware_backend/app.js
@@ -35,12 +35,12 @@ function databaseQuery(res = null, query) {
 }
 
 function databaseCreateTables(res = null) {
-    var sql = "CREATE TABLE patient_data (ID INT AUTO_INCREMENT PRIMARY KEY, NHI VARCHAR(255), FirstName VARCHAR(255), LastName VARCHAR(255), MiddleName VARCHAR(255), DOB DATE)";
+    var sql = "CREATE TABLE patient_data (ID INT AUTO_INCREMENT PRIMARY KEY, NHI VARCHAR(255), FirstName VARCHAR(255), LastName VARCHAR(255), MiddleName VARCHAR(255), DOB DATE, ContactNumber INT, Email VARCHAR(255))";
     databaseQuery(res, sql)
-    sql = "CREATE TABLE bookings (ID INT AUTO_INCREMENT PRIMARY KEY, Date date, Patient int, Notes VARCHAR(255), FOREIGN KEY (Patient) REFERENCES patient_data(ID))";
-    databaseQuery(res, sql)
-    sql = "CREATE TABLE accounts (ID INT AUTO_INCREMENT PRIMARY KEY, AccountName VARCHAR(255), AccountPasswordHash VARCHAR(255), AccountPasswordSalt VARCHAR(255), AccountAccessLevel INT)";
+    sql = "CREATE TABLE accounts (ID INT AUTO_INCREMENT PRIMARY KEY, AccountName VARCHAR(255), AccountPasswordHash VARCHAR(255), AccountPasswordSalt VARCHAR(255), AccountAccessLevel INT, DentistNumber INT, DOB DATE, Email VARCHAR(255), PhoneNumber INT)";
     databaseQuery(null, sql) 
+    sql = "CREATE TABLE bookings (ID INT AUTO_INCREMENT PRIMARY KEY, Date DATE, Patient INT, Dentist INT, Type VARCHAR(255), FeeDollars INT, FeeCents INT, Location VARCHAR(255), Notes VARCHAR(255), FOREIGN KEY (Patient) REFERENCES patient_data(ID), FOREIGN KEY (Dentist) REFERENCES accounts(ID))";
+    databaseQuery(res, sql)
 }
 
 module.exports = app;
