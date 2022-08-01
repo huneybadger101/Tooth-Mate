4564b92720c4ebbf35f231eaf843906f427885fc
GQR726 <223664305gqr@gmail.com>
2022-08-01 14:57:40 +1200

    open window

--- dentalsoftware_frontend/src/components/readjosn/fileread.tsx
+++ dentalsoftware_frontend/src/components/readjosn/fileread.tsx
@@ -1,6 +1,6 @@
 import { Text, View, Button, LineEdit } from "@nodegui/react-nodegui";
 import React from "react";
-import {  QFileDialog } from "@nodegui/nodegui";
+import {  QFileDialog,FileMode,QMainWindow,QWidget } from "@nodegui/nodegui";
 
 export class Reading extends React.Component<any, any> {
 
@@ -13,9 +13,16 @@ export class Reading extends React.Component<any, any> {
     render() {
         const buttonHandler = {
             clicked: () => {
+
               const fileDialog = new QFileDialog();
+              fileDialog.setFileMode(FileMode.AnyFile);
+              fileDialog.setNameFilter('text (*txt)');
+              fileDialog.exec();
+              
               const selectedFiles = fileDialog.selectedFiles();
-              console.log(selectedFiles);
+              // const result = fileDialog.accessibleDescription();
+              console.log(selectedFiles.map); 
+              // console.log(result); 
 
               this.setState({
                 text: selectedFiles
@@ -23,12 +30,26 @@ export class Reading extends React.Component<any, any> {
             }
         };
 
+        const MainWindow = {
+          clicked: () => {
+
+            const win = new QMainWindow();
+
+            const centralWidget = new QWidget();
+            win.setCentralWidget(centralWidget);
+            win.setMinimumSize(600,600);
+            win.show();
+
+            // global.win = win;
+          }
+        };
+
         return (
             
             <View style="flex: 1; background-color: 'grey';">
 
                 <Button text = {"Choose text File"} on = {buttonHandler} id={"btn"}/>
-
+                <Button text = {"open a Window"} on = {MainWindow} id={"btn"}/>
                 <Text>{this.state.text}</Text>
 
             </View>
