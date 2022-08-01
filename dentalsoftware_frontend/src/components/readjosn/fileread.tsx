dc3853e99f2db4840bb7adf7a8836fb6bf253fde
GQR726 <223664305gqr@gmail.com>
2022-08-01 20:16:59 +1200

    alert system
    done

--- dentalsoftware_frontend/src/components/readjosn/fileread.tsx
+++ dentalsoftware_frontend/src/components/readjosn/fileread.tsx
@@ -1,12 +1,14 @@
 import { Text, View, Button, LineEdit } from "@nodegui/react-nodegui";
 import React from "react";
-import {  QFileDialog,FileMode,QMainWindow,QWidget } from "@nodegui/nodegui";
+import {  QFileDialog,FileMode,QMainWindow,QWidget,FlexLayout,QLabel,QPushButton } from "@nodegui/nodegui";
 
 export class Reading extends React.Component<any, any> {
 
     constructor(props: any) {
         super(props);
         this.state = {
+          Numa: 0,
+          Numb: 0,
           text: ''
         }
       }
@@ -33,11 +35,27 @@ export class Reading extends React.Component<any, any> {
         const MainWindow = {
           clicked: () => {
 
+            var Num1 = parseInt(this.state.Numa, 10);
+            var Num2 = parseInt(this.state.Numb, 10);
+            var Num3 = Num1+Num2;
+
             const win = new QMainWindow();
 
             const centralWidget = new QWidget();
+            const layout = new FlexLayout();
+            centralWidget.setObjectName("container");
+            centralWidget.setLayout(layout);
+
+            const label = new QLabel();
+            label.setText("The answer is "+Num3+" !");
+            const button = new QPushButton();
+            button.setText("close");
+            button.addEventListener('clicked',(checked)=>win.close());
+            layout.addWidget(label);
+            layout.addWidget(button);
+
             win.setCentralWidget(centralWidget);
-            win.setMinimumSize(600,600);
+            win.setMinimumSize(200,0);
             win.show();
 
             // global.win = win;
@@ -49,7 +67,24 @@ export class Reading extends React.Component<any, any> {
             <View style="flex: 1; background-color: 'grey';">
 
                 <Button text = {"Choose text File"} on = {buttonHandler} id={"btn"}/>
-                <Button text = {"open a Window"} on = {MainWindow} id={"btn"}/>
+
+                <View style="flex-direction: row; justify-content: start; align-items: start;">
+                    <LineEdit on={{ textChanged: (textValue) => {
+                        this.setState({
+                          Numa: textValue.replace(/[^0-9! ]+/g, '')
+                        })
+                    } }} text={this.state.Numa} />
+                </View>
+
+                <View style="flex-direction: row; justify-content: start; align-items: start;">
+                    <LineEdit on={{ textChanged: (textValue) => {
+                        this.setState({
+                          Numb: textValue.replace(/[^0-9! ]+/g, '')
+                        })
+                    } }} text={this.state.Numb} />
+                </View>
+
+                <Button text = {"Add('+')"} on = {MainWindow} id={"btn"}/>
                 <Text>{this.state.text}</Text>
 
             </View>
