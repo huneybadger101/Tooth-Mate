fa844c6cc81936fe6c2be2bea09e770b1af49650
terkrub <terkrub9@gmail.com>
2023-08-03 01:43:29 +1200

    header of page

---
+++ toothmate_current_team/src/Components/PopupPage/TopHeaderToothName.jsx
@@ -0,0 +1,11 @@
+import'./StylesSheets/TopHeaderToothNameStyles.css'
+
+const TopHeaderToothName=()=>{
+    return(
+        <div className="toothName">
+            <a>Tooth Name{'(Tooth CODE)'}</a>
+        </div>
+    )
+}
+
+export default TopHeaderToothName
