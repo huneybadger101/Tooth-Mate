10b7f2dc72df665f23c4d434feccf7a747fc3394
terkrub <terkrub9@gmail.com>
2023-08-03 01:42:53 +1200

    add model container

---
+++ toothmate_current_team/src/Components/PopupPage/IndividualModel.jsx
@@ -0,0 +1,11 @@
+import './StylesSheets/individualModelStyles.css'
+
+const IndividualModel=()=>{
+    return(
+        <div className="individualModel">
+            <a>waiting for individual Model</a>
+        </div>
+    )
+}
+
+export default IndividualModel
