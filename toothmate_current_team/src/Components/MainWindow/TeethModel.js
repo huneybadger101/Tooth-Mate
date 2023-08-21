860f0295908af696ca3c87aced052178998d69f2
An1honyY <ant.jo.yuen@gmail.com>
2023-08-22 00:54:10 +1200

    Add Updating TeethModel content.
    I removed the individual js files for the buttons. They are all created inside the TeethModel file now.

--- toothmate_current_team/src/Components/MainWindow/TeethModel.js
+++ toothmate_current_team/src/Components/MainWindow/TeethModel.js
@@ -1,4 +1,8 @@
+import React, { useState } from 'react';
 import '../../StyleSheets/MainWindow/TeethModel.css';
+import '../../StyleSheets/MainWindow/BasePlanButton.css';
+import '../../StyleSheets/MainWindow/TreatmentPlanButton.css';
+import '../../StyleSheets/MainWindow/PeriPlanButton.css';
 import Tooth from './Tooth.js';
 import PeriPopup from '../PeridontalPopup/PeriPopup';
 
@@ -8,14 +12,41 @@ import PeriPopup from '../PeridontalPopup/PeriPopup';
  * @returns Teeth Model
  */
 function TeethModel() {
+    const [activeContent, setActiveContent] = useState('contentBase');  // This is the default content that will be set as active.
+
+    const handleContentChange = (contentKey) => {
+        setActiveContent(contentKey);
+    };
+
+    const contentMap = {
+        contentBase: <div><Tooth /></div>,
+        contentTreatment: <div>Treatment Plan</div>,
+        contentPeri: <div><PeriPopup /></div>,
+    };
+
     return (
-        <div className='grid-layout'>
-            <div className="teeth-model-container">
-                {null}
-                <Tooth />
-                <PeriPopup />
+        <>
+            <div className='grid-layout'>
+                <div className="teeth-model-container" activeContent={activeContent} handleContentChange={handleContentChange}>
+                    {contentMap[activeContent]}
+                </div>
             </div>
-        </div>
+            <div className="grid-layout">
+                <div className="base-plan-button-container">
+                    <button className="base-plan-button" onClick={() => handleContentChange('contentBase')}>B</button>
+                </div>
+            </div>
+            <div className="grid-layout">
+                <div className="treatmentplan-button-container">
+                    <button className="treatmentplan-button" onClick={() => handleContentChange('contentTreatment')}>T</button>
+                </div>
+                <div className="grid-layout">
+                    <div className="periplan-button-container">
+                        <button className="periplan-button" onClick={() => handleContentChange('contentPeri')}>Peri</button>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
