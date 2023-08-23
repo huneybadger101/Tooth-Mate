e5d3aeb25fdd0fca6bb596714bc349000162aa2f
An1honyY <ant.jo.yuen@gmail.com>
2023-08-23 20:37:44 +1200

    Added Draggable, draggable all sides

--- toothmate_current_team/src/Components/PeridontalPopup/PeriPopup.js
+++ toothmate_current_team/src/Components/PeridontalPopup/PeriPopup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Draggable from 'react-draggable';
 import '../../StyleSheets/PeridontalPopup/PeriPopup.css';
 
 function PeriPopup() {
@@ -11,15 +12,21 @@ function PeriPopup() {
             {/* && works the same as if statement in jsx*/}
             {isPopupVisible && (
                 <div className="peri-popup-container">
-                    
+                    <Draggable handle=".handle">
                         <div className="peri-popup-content">
+                            {/* Draggable Handles Start*/}
+                            <div className="handle handle-top"></div>
+                            <div className="handle handle-bottom"></div>
+                            <div className="handle handle-right"></div>
+                            <div className="handle handle-left"></div>
+                            {/* Draggable Handles End*/}
                             <p>Peridontal View - Toothname</p>
                             <div className="peri-model">Peri Model</div>
                             <div className="peri-history">Peri History</div>
                             <div className="peri-info">Peri Info</div>
                             <img src='icons/x-square.svg' alt="Close Icon" onClick={() => setPopupVisible(false)} className='close' />
                         </div>
-                   
+                    </Draggable>
                 </div>
             )}
 
