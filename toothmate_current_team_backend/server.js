e76e870d26fcef6607599d149e4ef8a08b6fa557
terkrub <terkrub9@gmail.com>
2023-08-29 07:49:28 +1200

    add the pull patient history

--- toothmate_current_team_backend/server.js
+++ toothmate_current_team_backend/server.js
@@ -36,6 +36,11 @@ app.get('/:nhi', (req,res) =>{
             connection.query("SELECT * FROM contacts WHERE id = ?", [contactId], (error, secondResults, fields) => {
                 if (error) throw error;
                 console.log(secondResults);
+                connection.query("SELECT * FROM appointments WHERE NHI = ? ORDER BY appointment_date ASC", [req.params.nhi], (error, thirdResults, fields) => {
+                    if (error) throw error;
+                    console.log(secondResults);
+                    res.json({status:"Success", info: secondResults, otherInfo: firstResults, history: thirdResults})
+                });                
                 res.json({status:"Success", info: secondResults, otherInfo: firstResults})
             });
         }
