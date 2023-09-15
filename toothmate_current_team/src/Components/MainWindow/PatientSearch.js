91eff6ad10fe8ce63505a173cda545528a6a2b60
huneybadger101 <84160599+huneybadger101@users.noreply.github.com>
2023-09-15 14:01:17 +1200

    menu bar updates

--- toothmate_current_team/src/Components/MainWindow/PatientSearch.js
+++ toothmate_current_team/src/Components/MainWindow/PatientSearch.js
@@ -5,9 +5,9 @@ function NHISearch({setActiveContent}) {
     return (
         <>
         <div className="nhi-search-container">
-        <PlanSwitchButtons
-          handleContentChange={setActiveContent}
-        />
+            
+        <PlanSwitchButtons handleContentChange={setActiveContent}/>
+
             <form className="nhi-search" id="NHI">
                 <input type="text" placeholder="NHI"/>
                 <input type="text" placeholder = "Name"/>
