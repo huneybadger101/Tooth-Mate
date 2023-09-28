3067034a68950954c0f010b071770a2f0a141f95
terkrub <terkrub9@gmail.com>
2023-09-29 11:35:29 +1300

    like the updated patient info with backend

--- toothmate_current_team/src/Components/MainWindow/PatientInfo.js
+++ toothmate_current_team/src/Components/MainWindow/PatientInfo.js
@@ -1,25 +1,111 @@
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 import '../../StyleSheets/MainWindow/PatientInfo.css';
 
-function PatientInfo({ patientData, formatDate}) {
-    //To to display the information just go to have a look at console in browser then have a look in json
-    /*
-        then chose the info that u want to pull for example the first_name it in the patientInfo so
-        we will use any tag for example <a>{patientInfo.first_name}</a>
-    */
-    const patientInfo = patientData && patientData.info && patientData.info[0];
-    const otherInfo = patientData && patientData.otherInfo && patientData.otherInfo[0];
-    //console.log(otherInfo);
-    return (
+function PatientInfo({ patientData, formatDate }) {
+    const patientInfo = patientData?.info?.[0];
+    const otherInfo = patientData?.otherInfo?.[0];
+    const [edit, setEdit] = useState(false);
 
-        <div className="patient-info-container">
-            <h3>Patient Info</h3>
-            <div className="info-field" >
-                <p>Name: {patientInfo!=='N' && patientInfo?patientInfo.salutation+" "+patientInfo.first_name+" "+patientInfo.last_name:""}</p>
-                <p>Date of Birth: {otherInfo? formatDate(otherInfo.dob_c):""}</p>
-                <p>Address: {patientInfo?patientInfo.primary_address_street:""}, {patientInfo? patientInfo.primary_address_city:""}, {patientInfo? patientInfo.primary_address_country:""}</p>
-                <p>Phone: {patientInfo? patientInfo.phone_mobile:""}</p>
-            </div>
-        </div>
+    // State for input fields
+    const [firstName, setFirstName] = useState(patientInfo?.first_name??"");
+    const [lastName, setLastName] = useState(patientInfo?.last_name ?? "");
+    const [dob, setDob] = useState(otherInfo ? formatDate(otherInfo.dob_c) : "");
+    const [street, setStreet] = useState(patientInfo?.primary_address_street ?? "");
+    const [city, setCity] = useState(patientInfo?.primary_address_city ?? "");
+    const [country, setCountry] = useState(patientInfo?.primary_address_country ?? "");
+    const [phone, setPhone] = useState(patientInfo?.phone_mobile ?? "");
+
+    useEffect(() => {
+        if (patientData) {
+            const patientInfo = patientData?.info?.[0];
+            const otherInfo = patientData?.otherInfo?.[0];
+            
+            setFirstName(patientInfo?.first_name ?? "");
+            setLastName(patientInfo?.last_name ?? "");
+            setDob(otherInfo ? formatDate(otherInfo.dob_c) : "");
+            setStreet(patientInfo?.primary_address_street ?? "");
+            setCity(patientInfo?.primary_address_city ?? "");
+            setCountry(patientInfo?.primary_address_country ?? "");
+            setPhone(patientInfo?.phone_mobile ?? "");
+        }
+    }, [patientData]);
+
+    const handleSave=async()=>{
+        try {
+            const response = await axios.post('https://5f34ab754164.ngrok.app/updateInfo', {
+              firstName: firstName,
+              lastName: lastName,
+              dob:dob,
+              street:street,
+              city:city,
+              country:country,
+              phone:phone
+            });
+            
+            alert('Update patient info successfully!')
+          } catch (error) {
+            console.error('There was an error adding the record!', error);
+          }
+    }
+
+    const handleCancel=()=>{
+        setFirstName(patientInfo?.first_name ?? "");
+        setLastName(patientInfo?.last_name ?? "");
+        setDob(otherInfo ? formatDate(otherInfo.dob_c) : "");
+        setStreet(patientInfo?.primary_address_street ?? "");
+        setCity(patientInfo?.primary_address_city ?? "");
+        setCountry(patientInfo?.primary_address_country ?? "");
+        setPhone(patientInfo?.phone_mobile ?? "");
+        setEdit(false)
+    }
+
+    return (
+        <>
+            {edit ? (
+                <div className="patient-info-container-edit">
+                    <h3>Patient Info</h3>
+                    <div className="info-field">
+                        <label>First name:</label>
+                        <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                        <br/>
+                        <label>Last name:</label>
+                        <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                        <br/>
+                        <label>DOB:</label>
+                    <input type="date" value={dob} onChange={(e) => setDob(e.target.value)} />
+                        <br/>
+                        <label>Address:</label>
+                        <input value={street} onChange={(e) => setStreet(e.target.value)} />
+                        <br/>
+                        <label>City name:</label>
+                        <input value={city} onChange={(e) => setCity(e.target.value)} />
+                        <br/>
+                        <label>Country:</label>
+                        <input value={country} onChange={(e) => setCountry(e.target.value)} />
+                        <br/>
+                        <label>Phone number:</label>
+                        <input value={phone} onChange={(e) => setPhone(e.target.value)} />
+                    </div>
+                    <div className='btn-container'>
+                        <button className='save-btn' onClick={() => { handleSave() }}>SAVE</button>
+                        <button className='edit-btn' onClick={() => { handleCancel() }}>cancel</button>
+                    </div>
+                    
+                </div>
+            ) : (
+                <div className="patient-info-container">
+                    <h3>Patient Info</h3>
+                    <div className="info-field">
+                        <p>Name: {`${firstName} ${lastName}`}</p>
+                        <p>Date of Birth: {dob}</p>
+                        <p>Address: {`${street}, ${city}, ${country}`}</p>
+                        <p>Phone: {phone}</p>
+                    </div>
+                    <button className='edit-btn' onClick={() => { setEdit(true) }}>Edit</button>
+                </div>
+            )}
+        </>
     );
 }
 
