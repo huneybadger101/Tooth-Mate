75c23f1f99731ea0f33501db37f329e8264e2d43
terkrub <terkrub9@gmail.com>
2023-09-29 14:37:45 +1300

    fix the bug

--- toothmate_current_team/src/Components/MainWindow/SubmitButton.js
+++ toothmate_current_team/src/Components/MainWindow/SubmitButton.js
@@ -1,10 +1,10 @@
 import '../../StyleSheets/MainWindow/SubmitButton.css';
 
-function SubmitButton({addRecord}) {
+function SubmitButton({addRecord,save}) {
     return (
 
             <div className="submit-button-container">
-            <button type="button" className="submit-button" onClick={()=>{addRecord()}}>Save</button>
+            <button type="button" className="submit-button" onClick={()=>{addRecord()}}>{save?"Update":"Save"}</button>
             </div>
 
     );
