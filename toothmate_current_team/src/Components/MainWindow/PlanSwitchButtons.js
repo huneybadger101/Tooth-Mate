8d053716e168d812562c7707c1fca80212440ba6
An1honyY <ant.jo.yuen@gmail.com>
2023-10-26 22:41:50 +1300

    Update selected plan button highlighted

--- toothmate_current_team/src/Components/MainWindow/PlanSwitchButtons.js
+++ toothmate_current_team/src/Components/MainWindow/PlanSwitchButtons.js
@@ -1,15 +1,35 @@
-
-import '../../StyleSheets/MainWindow/PlanSwitchButtons.css'
-
+import React, { useState } from 'react';
+import '../../StyleSheets/MainWindow/PlanSwitchButtons.css';
 
 function PlanSwitchButtons(props) {
     const { handleContentChange } = props;
+    const [selectedPlan, setSelectedPlan] = useState('contentBase');
+
+    const handlePlanChangeAndUpdateSelection = (plan) => { 
+        handleContentChange(plan); // original handler
+        setSelectedPlan(plan); // Used to highlight the selected plan button
+    };
 
     return (
         <div className="plan-switch-container">
-                    <button onClick={() => handleContentChange('contentBase')}>Current</button>
-                    <button onClick={() => handleContentChange('contentTreatment')}>Treatment</button>
-                    <button onClick={() => handleContentChange('contentPeri')}>Periodontal</button>
+            <button
+                onClick={() => handlePlanChangeAndUpdateSelection('contentBase')}
+                className={selectedPlan === 'contentBase' ? 'selected' : ''}
+            >
+                Current
+            </button>
+            <button
+                onClick={() => handlePlanChangeAndUpdateSelection('contentTreatment')}
+                className={selectedPlan === 'contentTreatment' ? 'selected' : ''}
+            >
+                Treatment
+            </button>
+            <button
+                onClick={() => handlePlanChangeAndUpdateSelection('contentPeri')}
+                className={selectedPlan === 'contentPeri' ? 'selected' : ''}
+            >
+                Periodontal
+            </button>
         </div>
     );
 }
